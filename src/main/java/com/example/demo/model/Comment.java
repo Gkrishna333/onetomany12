f2fb27ecd3618526bbf0872247dbf20483b186f0
saikrishna <gadadas.saikrishna@gmail.com>
2022-09-05 16:52:23 +0530

    first commit

--- src/main/java/com/example/demo/model/Comment.java
+++ src/main/java/com/example/demo/model/Comment.java
@@ -1,15 +1,12 @@
 package com.example.demo.model;
 
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.Formula;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
